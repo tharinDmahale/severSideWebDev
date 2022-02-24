949968c7cdc722340780edb70871688ab840d5ac
Tharin Deelaka Mahale <tharin.20200709@iit.ac.lk>
2022-02-24 10:19:01 +0530

    improve clear basket page

--- homteq/clearbasket.php
+++ homteq/clearbasket.php
@@ -16,6 +16,9 @@ echo "<h4>".$pagename."</h4>";
 
 unset($_SESSION['basket']);
 
+echo "<p>Your basket was cleared!</p>";
+echo "<p>You may go back to the <a href='basket.php'>basket</a></p>";
+
 include("footfile.html");
 
 echo "</body>";
