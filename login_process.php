688779b43129748a3dfe3b978fc804d4f5a33058
Tharin Deelaka Mahale <tharin.20200709@iit.ac.lk>
2022-03-28 17:09:54 +0530

    post test

--- login_process.php
+++ login_process.php
@@ -2,6 +2,19 @@
 session_start();
 include("db.php");
 $pagename="Your Login Results";
+
+function authenticate($enteredPassword, $dbPassword)
+{
+    if ($enteredPassword == $dbPassword)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
 echo "<link rel=stylesheet type=text/css href=mystylesheet.css>";
 
 echo "<title>".$pagename."</title>";
@@ -12,7 +25,12 @@ include ("headfile.html");
 
 echo "<h4>".$pagename."</h4>";
 
-//n
+$email = $_POST['userEmailField'];
+$password = $_POST['userPasswordField'];
+
+// test post
+echo "<p>email: ".$email."</p>";
+echo "<p>password: ".$password."</p>";
 
 include("footfile.html");
 
