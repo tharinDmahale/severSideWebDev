d68496a2134812fdb40689452956223bca94fd30
Tharin Deelaka Mahale <tharin.20200709@iit.ac.lk>
2022-03-28 16:53:20 +0530

    add login

---
+++ homteq/login.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+$pagename="Sign Up";
+echo "<link rel=stylesheet type=text/css href=mystylesheet.css>";
+
+echo "<title>".$pagename."</title>";
+
+echo "<body>";
+
+include ("headfile.html");
+
+echo "<h4>".$pagename."</h4>";
+
+// login form
+echo "<table>";
+echo "<form action='login_process.php' method='post'>";
+
+echo "<tr>";
+echo "<td>";
+echo "<p>Email: </p>";
+echo "</td>";
+echo "<td>";
+echo "<input type='text' name='userEmailField'/>";
+echo "</td>";
+echo "</tr>";
+
+echo "<tr>";
+echo "<td>";
+echo "<p>Email: </p>";
+echo "</td>";
+echo "<td>";
+echo "<input type='text' name='userEmailField'/>";
+echo "</td>";
+echo "</tr>";
+
+echo "</form>";
+echo "</table>";
+
+include("footfile.html");
+
+echo "</body>";
+?>
